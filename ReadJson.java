0140b3df01a3ec8fbad2840dab412d1df72b5622
rox <roxmarzan14@gmail.com>
2023-04-21 15:20:33 +0800

    Added files and json integration

---
+++ ReadJson.java
@@ -0,0 +1,51 @@
+package main;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ReadJson {
+//	global vars
+	static String roomFName, roomSPrice, roomIconImg, roomDesc, roomHeroIcon, roomOtherImageInfo, dummyheroIcon;
+	static long roomPrice;
+	
+    public static void fetchData(String roomData) {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("/Users/jaynardvillarisco/Documents/folders/test-json/data.json")) {
+            Object obj = jsonParser.parse(reader);
+            JSONArray roomList = (JSONArray) obj;
+
+            for (int i = 0; i < roomList.size(); i++) {
+                JSONObject room = (JSONObject) roomList.get(i);
+                String roomName = (String) room.get("room");
+                
+                if (roomName.equals(roomData)) {
+                    JSONArray features = (JSONArray) room.get("feature");
+                    JSONObject images = (JSONObject) room.get("images");
+                    
+                    roomFName = (String) room.get("roomName");
+                    roomPrice = (long) room.get("price");
+                    roomSPrice = (String) room.get("priceS");
+                    roomIconImg = (String) images.get("smallImageIcon");
+                    roomHeroIcon = (String) images.get("heroIcon");
+                    roomOtherImageInfo = (String) images.get("otherImageInfo");
+                    dummyheroIcon = (String) images.get("dummyheroIcon");
+                    roomDesc = (String) room.get("desc");
+                    
+//                  loop for features
+                    for (int j = 0; j < features.size(); j++) {
+                        String feature = (String) features.get(j);
+                    }
+                }
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
